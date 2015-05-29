33d63512622075e0e745b66c2c92888d6c663444
hackrole <daipeng123456@gmail.com>
2015-05-29 16:17:03 +0800

    fix

--- scrum/scrum/board/static/board/js/models.js
+++ scrum/scrum/board/static/board/js/models.js
@@ -6,12 +6,12 @@
 
     function getCookie(name){
         var cookieValue = null;
-        if (document.cookie && document.cookie != ''){
+        if (document.cookie && document.cookie !== ''){
             var cookies = document.cookie.split(';');
             for (var i=0; i < cookies.length; i++){
                 var cookie = $.trim(cookies[i]);
 
-                if(cookie.substring(0, name.length + 1) == (name + '=')){
+                if(cookie.substring(0, name.length + 1) === (name + '=')){
                     cookieValue = decodeURIComponent(cookie.substring(name.length+1));
                     break;
                 }
@@ -55,9 +55,9 @@
             this.save(null);
         },
         authenticated: function(){
-            return this.get('token') != null;
+            return this.get('token') !== null;
         },
-        _setupAUth: function(settings, originalOptions, xhr){
+        _setupAuth: function(settings, originalOptions, xhr){
             if(this.authenticated()){
                 xhr.setRequestHeader(
                     'Authorization',
